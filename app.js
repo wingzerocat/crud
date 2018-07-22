ed6e6d50002befe4fc428f79bc6b575589f55f39
Michael Lou <michaellou@Michaels-MacBook-Pro.local>
2018-07-22 12:01:07 -0500

    Trying to show more contact details

--- app.js
+++ app.js
@@ -11,16 +11,28 @@ $(document).ready(function() {
   const displayResults = function(key) {
     let name = JSON.parse(key);
     let info = JSON.parse(localStorage.getItem(key));
-    let $result = $('<div><div class="temp"><div class="name"></div><div class="info"></div></div></div>');
-    $result.find('.name').text(name.firstName + ' ' + name.lastName);
-    //$result.find('.info').text(info.age);
-    $result.appendTo($results);
+    let $match = $('<div><div class="temp"><div class="name"></div><div class="info"></div></div></div>');
+    $match.find('.name').text(name.firstName + ' ' + name.lastName);
+    $match.find('.info').text(key);
+    $match.appendTo($results);
+  }
+
+  // Shows all contacts
+  const showAll = function() {
+    $results.html('');
+    $('.add-contact').css('visibility', 'visible');
+    $('.store-btn').css('visibility', 'hidden');
+    $('.crud-form').css('visibility', 'hidden');
+    $('.crud-form').hide();
+    for (let i = 0; i < localStorage.length; i++) {
+      displayResults(localStorage.key(i));
+    }
+    $results.show().slideDown();
   }
 
   const clearForm = function() {
     $('.first-name').val('');
     $('.last-name').val('');
-    $('.age').val('');
   }
 
 //STORE
@@ -37,93 +49,97 @@ $(document).ready(function() {
     localStorage.setItem(JSON.stringify(key), JSON.stringify(value));
 
     clearForm();
+    showAll();
   });
 
 
 //RETRIEVE
-  $('.get-btn').on('click', function(event) {
-    event.preventDefault();
-    $results.html('');
-
-    let keySearch = {};
-    let valueSearch = {};
-    let kFlag = false;
-    let sFlag = false;
-    let hits = 0;
-    //flags are to gate which if else statement it will go into
-
-    if ($('.first-name').val()) {
-      keySearch.firstName = $('.first-name').val();
-    }
-    if ($('.last-name').val()) {
-      keySearch.lastName = $('.last-name').val();
-    }
-    if ($('.age').val()) {
-      valueSearch.age = $('.age').val();
-    }
-
-    let kTerm = JSON.stringify(keySearch);
-    let sTerm = JSON.stringify(valueSearch);
-
-    if (kTerm.length > 2) {
-      kFlag = true;
-    }
-    if (sTerm.length > 2) {
-      sFlag = true;
-    }
-
-    for (let i = 0; i < localStorage.length; i++) {
-      if (kFlag && sFlag) {
-        if (localStorage.key(i).includes(kTerm.slice(1, kTerm.length - 1)) && localStorage.getItem(localStorage.key(i)).includes(sTerm.slice(1, sTerm.length - 1))) {
-          displayResults(localStorage.key(i), hits);
-          hits += 1;
-        }
-      } else if (kFlag) {
-        if (localStorage.key(i).includes(kTerm.slice(1, kTerm.length - 1))) {
-          displayResults(localStorage.key(i), hits);
-          hits += 1;
-        }
-      } else if (sFLag) {
-        if (localStorage.getItem(localStorage.key(i)).includes(sTerm.slice(1, sTerm.length - 1))) {
-          displayResults(localStorage.key(i), hits);
-          hits += 1;
-        }
-      }
-    }
-
-    let $hits = $('<div><div class="body"></div></div>');
-    $hits.find('.body').text(hits);
-    $hits.prependTo($results);
-
-    clearForm();
-  });
+  // $('.get-btn').on('click', function(event) {
+  //   event.preventDefault();
+  //   $results.html('');
+  //
+  //   let keySearch = {};
+  //   let valueSearch = {};
+  //   let kFlag = false;
+  //   let sFlag = false;
+  //   let hits = 0;
+  //   //flags are to gate which if else statement it will go into
+  //
+  //   if ($('.first-name').val()) {
+  //     keySearch.firstName = $('.first-name').val();
+  //   }
+  //   if ($('.last-name').val()) {
+  //     keySearch.lastName = $('.last-name').val();
+  //   }
+  //   if ($('.age').val()) {
+  //     valueSearch.age = $('.age').val();
+  //   }
+  //
+  //   let kTerm = JSON.stringify(keySearch);
+  //   let sTerm = JSON.stringify(valueSearch);
+  //
+  //   if (kTerm.length > 2) {
+  //     kFlag = true;
+  //   }
+  //   if (sTerm.length > 2) {
+  //     sFlag = true;
+  //   }
+  //
+  //   for (let i = 0; i < localStorage.length; i++) {
+  //     if (kFlag && sFlag) {
+  //       if (localStorage.key(i).includes(kTerm.slice(1, kTerm.length - 1)) && localStorage.getItem(localStorage.key(i)).includes(sTerm.slice(1, sTerm.length - 1))) {
+  //         displayResults(localStorage.key(i), hits);
+  //         hits += 1;
+  //       }
+  //     } else if (kFlag) {
+  //       if (localStorage.key(i).includes(kTerm.slice(1, kTerm.length - 1))) {
+  //         displayResults(localStorage.key(i), hits);
+  //         hits += 1;
+  //       }
+  //     } else if (sFLag) {
+  //       if (localStorage.getItem(localStorage.key(i)).includes(sTerm.slice(1, sTerm.length - 1))) {
+  //         displayResults(localStorage.key(i), hits);
+  //         hits += 1;
+  //       }
+  //     }
+  //   }
+  //
+  //   let $hits = $('<div><div class="body"></div></div>');
+  //   $hits.find('.body').text(hits);
+  //   $hits.prependTo($results);
+  //
+  //   clearForm();
+  // });
 
 //DELETE
-  $('.delete-btn').on('click', function(event) {
-    event.preventDefault();
-    let key = {};
-    key.firstName = $('.first-name').val();
-    key.lastName = $('.last-name').val();
+  // $('.delete-btn').on('click', function(event) {
+  //   event.preventDefault();
+  //   let key = {};
+  //   key.firstName = $('.first-name').val();
+  //   key.lastName = $('.last-name').val();
+  //
+  //
+  //   localStorage.removeItem(JSON.stringify(key));
+  //
+  //   clearForm();
+  // });
 
-
-    localStorage.removeItem(JSON.stringify(key));
-
-    clearForm();
-  });
-
+//Click to bring up the new contacts form
   $('.add-contact').on('click', function() {
-    // $results.html('');
-    //
-    // let $temp = $('<div><div>Hi</div></div>');
-    // $temp.appendTo($results).hide().slideDown();
     $results.slideUp();
+    $('.add-contact').css('visibility', 'hidden').hide();
+    $('.store-btn').css('visibility', 'visible');
+    $('.crud-form').css('visibility', 'visible');
+    $('.crud-form').slideDown();
   });
 
-  const showAll = function() {
-    for (let i = 0; i < localStorage.length; i++) {
-      displayResults(localStorage.key(i));
-    }
-  }
+// Display clicked contact info
+  $(document).on('click', '.temp', function() {
+    $results.html('');
+    let key = $(this).closest('.temp').find('.info').text();
+    displayResults(key);
+  });
 
+//Initial population of $results
   showAll();
 });
